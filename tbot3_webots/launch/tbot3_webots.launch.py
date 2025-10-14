06e925f5148d2ea6b82b9c66648a160163a7a7c0
ktiggos <k.tiggos@gmail.com>
2025-10-14 19:09:35 +0300

    Update launch file
    Bool to use PROTO or URDF solutions
    robot_state_publisher
    URDFSpawner
    WebotsController

--- tbot3_webots/launch/tbot3_webots.launch.py
+++ tbot3_webots/launch/tbot3_webots.launch.py
@@ -9,11 +9,17 @@ from ament_index_python.packages import get_package_share_directory
 from launch.event_handlers import OnProcessExit, OnProcessIO
 
 PKG_NAME = "tbot3_webots"
+PROTO = False
 
 def generate_launch_description():
     pkg_dir = get_package_share_directory(PKG_NAME)
 
-    urdf_path = os.path.join(pkg_dir,'description/urdf',"turtlebot3_waffle.urdf")
+    if PROTO:
+        urdf_name = "tbot3_waffle_proto.urdf"
+    else:
+        urdf_name = "turtlebot3_waffle.urdf"
+    
+    urdf_path = os.path.join(pkg_dir,'description/urdf',urdf_name)
     with open(urdf_path,'r') as urdf:
         lines = urdf.readlines()
         robot_description = ''.join(line for line in lines if not line.strip().startswith('<?xml'))
@@ -25,9 +31,34 @@ def generate_launch_description():
         output='screen'
     )
 
+    robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description' : robot_description
+        }]
+    )
+
+    if not PROTO:
+        spawner = URDFSpawner(
+            name="turtlebot3_waffle",
+            urdf_path=urdf_path,
+        )
+
+    driver = WebotsController(
+        robot_name="turtlebot3_waffle",
+        parameters=[
+            {'robot_description' : urdf_path}
+        ]
+    )
+
     return launch.launch_description.LaunchDescription([
         webots,
         webots._supervisor,
+        robot_state_publisher,
+        spawner,
+        # driver,
         launch.actions.RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=webots,
