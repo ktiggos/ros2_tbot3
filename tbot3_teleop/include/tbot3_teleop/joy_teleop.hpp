ff837550583deeddc28d582be7e3f06567bf2f23
ktiggos <k.tiggos@gmail.com>
2025-11-07 04:06:35 +0200

    teleop: joy_teleop: Add twist publisher

--- tbot3_teleop/include/tbot3_teleop/joy_teleop.hpp
+++ tbot3_teleop/include/tbot3_teleop/joy_teleop.hpp
@@ -4,16 +4,27 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/joy.hpp>
+#include <rosidl_runtime_cpp/message_initialization.hpp>
+
+#define MAX_ANALOG 37000.0
+#define MAX_TRIGGER 1000.0
+
+typedef rosidl_runtime_cpp::MessageInitialization MessageInit;
 
 class JoyTeleop : public rclcpp::Node {
     public:
         JoyTeleop();
     private:
         void joyCB(const sensor_msgs::msg::Joy::SharedPtr msg);
+        void pubCB();
 
         sensor_msgs::msg::Joy::SharedPtr joy_msg_;
+        geometry_msgs::msg::Twist cmd_vel_msg;
+
         rclcpp::Logger logger {this->get_logger()};
-        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_;
+        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_;
+        rclcpp::TimerBase::SharedPtr pub_timer_;
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
 
 #endif 
--- tbot3_teleop/src/joy_teleop.cpp
+++ tbot3_teleop/src/joy_teleop.cpp
@@ -1,18 +1,38 @@
 #include "tbot3_teleop/joy_teleop.hpp"
 
 JoyTeleop::JoyTeleop() : Node("joy_teleop"){
-    sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
+    cmd_vel_msg.linear = geometry_msgs::msg::Vector3(
+        MessageInit::ZERO
+    );
+    cmd_vel_msg.angular = geometry_msgs::msg::Vector3(
+        MessageInit::ZERO
+    );
+
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
+        "/cmd_vel", 10
+    );
+
+    subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>(
         "joy",
         10,
         [this](const sensor_msgs::msg::Joy::SharedPtr msg) -> void {
             this->joyCB(msg);
         }
     );
+
+    pub_timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
+        [this]() -> void {
+            this->pubCB();
+        }
+    );
 }
 
 void JoyTeleop::joyCB(const sensor_msgs::msg::Joy::SharedPtr msg){
     this->joy_msg_ = msg;
-    RCLCPP_INFO(logger, "SUB");
+}
+
+void JoyTeleop::pubCB(){
+    publisher_->publish(cmd_vel_msg);
 }
 
 int main(int argc, char* argv[]){
