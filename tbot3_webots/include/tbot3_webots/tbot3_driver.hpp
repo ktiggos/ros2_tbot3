5601532bca705de5ba7ec62b7ed99901a442fb28
ktiggos <k.tiggos@gmail.com>
2025-11-20 03:33:39 +0200

    webots: driver.hpp: Add odom variables

--- tbot3_webots/include/tbot3_webots/tbot3_driver.hpp
+++ tbot3_webots/include/tbot3_webots/tbot3_driver.hpp
@@ -4,9 +4,15 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/macros.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <tf2/LinearMath/Quaternion.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <rosidl_runtime_cpp/message_initialization.hpp>
 
 #include <webots_ros2_driver/WebotsNode.hpp>
 #include <webots_ros2_driver/PluginInterface.hpp>
+
+typedef rosidl_runtime_cpp::MessageInitialization MessageInit;
 
 namespace tb3_driver
 {
@@ -20,13 +26,21 @@ private:
 
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
     geometry_msgs::msg::Twist cmd_vel_msg;
+    nav_msgs::msg::Odometry odom_msg;
 
     WbDeviceTag left_motor;
     WbDeviceTag right_motor;
+    WbDeviceTag lm_sensor;
+    WbDeviceTag rm_sensor;
 
     // Callback watchdog
     rclcpp::Time cb_time, driver_time;
     double timeout{0.2}; // 200ms
+
+    // Odometry measurements
+    double ldis_last{0.0};
+    double rdis_last{0.0};
+    double theta{0.0};
 
     webots_ros2_driver::WebotsNode* node_;
 };
