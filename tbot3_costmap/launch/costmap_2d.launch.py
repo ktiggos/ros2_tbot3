f9e33a38aed783559d7af5f86b47c054bc859bf5
ktiggos <k.tiggos@gmail.com>
2026-04-06 15:27:57 +0300

    costmap: Launch costmap as lifecycle node type

--- tbot3_costmap/launch/costmap_2d.launch.py
+++ tbot3_costmap/launch/costmap_2d.launch.py
@@ -1,8 +1,11 @@
 import os
 import launch
 import launch_ros
+import lifecycle_msgs.msg
 
 from ament_index_python.packages import get_package_share_directory
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch_ros.event_handlers import OnStateTransition
 
 def generate_launch_description():
 
@@ -12,9 +15,11 @@ def generate_launch_description():
         "costmap.yaml"
     )
 
-    costmap_2d = launch_ros.actions.Node(
+    costmap_2d = launch_ros.actions.LifecycleNode(
         package="nav2_costmap_2d",
         executable="nav2_costmap_2d",
+        name="costmap",
+        namespace="",
         output="screen",
         parameters=[costmap_params]
     )
