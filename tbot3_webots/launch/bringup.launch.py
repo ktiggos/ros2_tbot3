393b7b36b5e8d00f53187c213c875d129c76cc72
ktiggos <k.tiggos@gmail.com>
2026-04-19 23:27:13 +0300

    bringup: Add teleop node

--- tbot3_webots/launch/bringup.launch.py
+++ tbot3_webots/launch/bringup.launch.py
@@ -8,11 +8,18 @@ from launch.event_handlers import OnProcessStart
 
 def generate_launch_description():
     pkg_webots = get_package_share_directory("tbot3_webots")
+    pkg_teleop = get_package_share_directory("tbot3_teleop")
     pkg_costmap = get_package_share_directory("tbot3_costmap")
 
     simulation = launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_webots, "launch", "tbot3_webots_proto.launch.py")
+        )
+    )
+
+    teleop = launch .actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_teleop, "launch", "joy_teleop.launch.py")
         )
     )
 
@@ -24,6 +31,7 @@ def generate_launch_description():
 
     return launch.launch_description.LaunchDescription([
         simulation,
+        teleop,
         launch.actions.TimerAction(
             period=6.0,
             actions=[costmap]
